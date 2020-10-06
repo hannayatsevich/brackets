4682a75047165d587fb6484ea8d360825cc37bad
hannayatsevich <hannayatsevich@gmail.com>
2020-10-06 15:28:05 +0300

    feat: func for defining true brackets sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openedBrackets = [];
+  let unpaired = [];
+  str.split('').forEach( element => {
+    let opening = bracketsConfig.find( bracketPair => element === bracketPair[0]);
+    let closing = bracketsConfig.find( bracketPair => element === bracketPair[1])
+    opening
+    ? (opening[0] === opening[1] && element === openedBrackets[openedBrackets.length - 1] ? openedBrackets.pop() : openedBrackets.push(element))
+    : (closing[0] === openedBrackets[openedBrackets.length - 1] ? openedBrackets.pop() : unpaired.push(element) );
+  })
+  return !openedBrackets.length && !unpaired.length;
 }
